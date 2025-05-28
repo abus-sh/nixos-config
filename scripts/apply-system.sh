3f8bd44aead7becaed933a2247dcd5b5f6579b2e
Abus <58699376+abus-sh@users.noreply.github.com>
2025-05-28 09:21:20 -0400

    Updated apply-system.sh to allow running directly as root

--- scripts/apply-system.sh
+++ scripts/apply-system.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
+DIR="${SUDO_HOME:-$HOME}"
+
 # Based on https://www.youtube.com/watch?v=Dy3KHMuDNS8
-pushd ~/.nixos
+pushd $DIR/.nixos
 sudo nixos-rebuild switch -I nixos-config=./system/configuration.nix
 popd
