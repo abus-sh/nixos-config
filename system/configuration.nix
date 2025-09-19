5c92b022f202a035f96fce0f1f58c091a14a318e
Abus <58699376+abus-sh@users.noreply.github.com>
2025-09-19 12:51:35 -0400

    Installed OBS

--- system/configuration.nix
+++ system/configuration.nix
@@ -180,6 +180,26 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  # OBS Studio
+  programs.obs-studio = {
+    enable = true;
+
+    # Nvidia hardware acceleration
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-backgroundremoval
+      obs-gstreamer
+      obs-vaapi
+      obs-vkcapture
+      wlrobs
+    ];
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
