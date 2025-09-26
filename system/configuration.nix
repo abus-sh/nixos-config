2ba4be547262e0ee888be8cb5c7931a377690c6e
Abus <58699376+abus-sh@users.noreply.github.com>
2025-09-26 19:28:06 -0400

    Removed xserver

--- system/configuration.nix
+++ system/configuration.nix
@@ -102,10 +102,6 @@ in
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
