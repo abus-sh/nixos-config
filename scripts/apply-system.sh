6834259d8034ca7db59398de759014b40db04500
Abus <58699376+abus-sh@users.noreply.github.com>
2025-09-26 23:53:58 -0400

    Added more help and upgrade options to apply-system.sh

--- scripts/apply-system.sh
+++ scripts/apply-system.sh
@@ -1,12 +1,66 @@
 #!/bin/sh
 
+usage () {
+    cat << EOF
+Usage: $0 [OPTIONS]
+
+Options:
+  -h, --help        Display this help message and exit.
+  -n, --name [NAME] The name of the config that will be used. Defaults to the
+                    hostname of the current computer if not specified.
+      --upgrade     Passes "--upgrade" to nixos-rebuild.
+      --upgrade-all Passes "--upgrade-all" to nixos-rebuild.
+EOF
+    exit ${1:-0}
+}
+
 # Function to append a flag to the FLAGS variable
 append_flag () {
     FLAGS="${FLAGS} ${1}"
 }
 
+handle_name() {
+    if [ -n "$name" ]; then
+        echo "Name may not be set multiple times."
+        usage 1
+    fi
+    name="$1"
+    echo "TODO: figure out flag to pass to use the given hostname"
+}
+
+handle_upgrade() {
+    append_flag "--upgrade"
+}
+
+handle_upgrade_all() {
+    append_flag "--upgrade-all"
+}
+
 # Append flag for the location of the system configuration
 append_flag "-I nixos-config=./system/configuration.nix"
+
+# Handle arguments to the script
+# Based on https://stackoverflow.com/a/31443098
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    # Short names
+    -h) usage; shift 1;;
+    -n) handle_name "$2"; shift 2 2>/dev/null;;
+
+    # Long names
+    --help) usage; shift 1;;
+    --name) handle_name "$2"; shift 2 2>/dev/null;;
+    --upgrade) handle_upgrade; shift 1;;
+    --upgrade-all) handle_upgrade_all; shift 1;;
+    
+    #-*) echo "Unknown option: $1" >&2; usage 1;;
+    #*) handle_argument "$1"; shift 1;;
+    *) echo "Invalid argument: $1" >&2; usage 1;;
+  esac
+  if [ $? -ne 0 ]; then
+    usage 1
+  fi
+done
 
 # Conditionally append flag to keep the current specialisation
 VERSION="$(cat /run/current-system/nixos-version)"
@@ -17,21 +71,6 @@ if [[ "$VERSION" =~ "-" ]]; then
 
     append_flag "-c ${SPEC}"
 fi
-
-# Handle arguments to the script
-# Based on https://superuser.com/a/186279
-while test $# -gt 0
-do
-    case "$1" in
-        --upgrade) append_flag "--upgrade"
-            ;;
-        --*) echo "Bad option: $1"
-            ;;
-        #*) echo "Argument $1"
-        #    ;;
-    esac
-    shift
-done
 
 # Make sure the right directory is used. If run as root, $SUDO_HOME will point to the right location, not $HOME.
 DIR="${SUDO_HOME:-$HOME}"
