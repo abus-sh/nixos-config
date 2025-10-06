fec8810f9ff3f7845e3113b8a31fcef3f201ee3b
Abus <58699376+abus-sh@users.noreply.github.com>
2025-10-06 14:22:41 -0400

    Added deploy-rs config for nixosvm

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, deploy-rs }: {
     nixosConfigurations.abusmachine = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./machines/abusmachine/configuration.nix ];
@@ -12,5 +13,19 @@
       system = "x86_64-linux";
       modules = [ ./machines/nixosvm/configuration.nix ];
     };
+
+    deploy.nodes.nixosvm = {
+      hostname = "nixosvm";
+      profiles.system = {
+        profiles.system = {
+          sshUser = "abus";
+          user = "root";
+          interactiveSudo = true;
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nixosvm;
+        };
+      };
+    };
+
+    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
   };
 }
