765b746c2e2b599e9d1dde484aec309a4a1824d0
Abus <git@abus.sh>
2025-10-07 02:01:09 -0400

    chore(artemis): remove old comments from configuration.nix

--- machines/artemis/configuration.nix
+++ machines/artemis/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
@@ -25,34 +21,13 @@
   # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
   # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -61,53 +36,11 @@
     pulse.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Account for deploy-rs
-  #users.users.deploy-rs = {
-  #  isSystemUser = true;
-  #  description = "deploy-rs system account";
-  #  group = "deploy-rs";
-  #  useDefaultShell = true;
-  #  openssh.authorizedKeys.keys = [
-  #    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKXIt2CN5GRiZ949+rmt48J8I57y9VpEEGTTllXBg8Pa abus@abusmachine"
-  #  ];
-  #};
-
-  #users.groups.deploy-rs = {};
-
-  #security.sudo.extraRules = [
-  #  {
-  #    users = [ "deploy-rs" ];
-  #    commands = [
-  #      {
-  #        command = "ALL";
-  #        options = [ "NOPASSWD" ];
-  #      }
-  #    ];
-  #  }
-  #];
-
-  #nix.settings.trusted-users = [ "deploy-rs" ];
-
-  # programs.firefox.enable = true;
-
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     vim
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh = {
@@ -118,16 +51,8 @@
     };
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
+  # Configure the firewall
   networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
