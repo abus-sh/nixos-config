2ce2d0b652562741e968079d060073e6ebb63424
Abus <git@abus.sh>
2025-11-02 18:27:23 -0500

    fix(boreas): allow unfree zerotierone package

--- machines/boreas/configuration.nix
+++ machines/boreas/configuration.nix
@@ -48,6 +48,10 @@
     enable = true;
   };
 
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.strings.getName pkg) [
+    "zerotierone"
+  ];
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
