8d451721d0f4e1eed2be2e422b62f6ba432c0304
Abus <git@abus.sh>
2026-05-27 14:40:24 -0400

    feat(abusmachine): add agenix cli

--- flake.nix
+++ flake.nix
@@ -10,6 +10,9 @@
       modules = [
         ./machines/abusmachine/configuration.nix
         agenix.nixosModules.default
+        {
+          environment.systemPackages = [ agenix.packages."x86_64-linux".default ];
+        }
       ];
     };
 
