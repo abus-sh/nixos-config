b6630588a336cd5efcaedb57500a529d5d5a1a6f
Abus <git@abus.sh>
2026-05-27 13:02:59 -0400

    feat(secrets): add agenix to all machines

--- flake.nix
+++ flake.nix
@@ -2,26 +2,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     deploy-rs.url = "github:serokell/deploy-rs";
+    agenix.url = "github:ryantm/agenix";
   };
-  outputs = { self, nixpkgs, deploy-rs }: {
+  outputs = { self, nixpkgs, deploy-rs, agenix }: {
     nixosConfigurations.abusmachine = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./machines/abusmachine/configuration.nix ];
+      modules = [
+        ./machines/abusmachine/configuration.nix
+        agenix.nixosModules.default
+      ];
     };
 
     nixosConfigurations.artemis = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
-      modules = [ ./machines/artemis/configuration.nix ];
+      modules = [
+        ./machines/artemis/configuration.nix
+        agenix.nixosModules.default
+      ];
     };
 
     nixosConfigurations.boreas = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./machines/boreas/configuration.nix ];
+      modules = [
+        ./machines/boreas/configuration.nix
+        agenix.nixosModules.default
+      ];
     };
 
     nixosConfigurations.nixosvm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./machines/nixosvm/configuration.nix ];
+      modules = [
+        ./machines/nixosvm/configuration.nix
+        agenix.nixosModules.default
+      ];
     };
 
     deploy.nodes.artemis = {
