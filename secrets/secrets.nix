34ed9e81c0a1ad1516dcd221f2b623002977a372
Abus <git@abus.sh>
2026-05-27 16:32:52 -0400

    feat(boreas): add boreas ssh key to secrets.nix

---
+++ secrets/secrets.nix
@@ -0,0 +1,28 @@
+let
+  abus = "";
+  users = [
+    abus
+  ];
+
+  artemis = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuYjqZKmwoow1vB9EHQE3UoEyCEJRlbHiEwbgE8mvnR";
+  boreas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK4OVO2P010PkpIQXwh511UUuAdggwd/k4snjxeo4JWQ";
+  nixosvm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHSzouiTkUJl7owycM0uq6ysGxI+MzjShCltpokSceLS";
+  systems = [
+    nixosvm
+  ];
+in
+{
+  "secret1.age".publicKeys = [
+    abus
+    nixosvm
+  ];
+  "secret2.age".publicKeys = users ++ systems;
+  "armored-secret.age" = {
+    publicKeys = [
+      abus
+    ];
+    armor = [
+      true
+    ];
+  };
+}
