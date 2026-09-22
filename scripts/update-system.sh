130dac8755c16b106f1dfaccc42a12818edd6ed9
Abus <git@abus.sh>
2026-09-21 21:24:33 -0400

    feat(abusmachine): update vscode extensions from update script

--- scripts/update-system.sh
+++ scripts/update-system.sh
@@ -1,5 +1,16 @@
 sudo nix-channel --update
 
 pushd ~/.nixos
+
 nix flake update
+
+# Update VS Code extension overlay rev
+REV=$(git ls-remote https://github.com/nix-community/nix-vscode-extensions | \
+  grep -F "refs/heads/master" | cut -f 1)
+if [ "${#REV}" -eq 40 ]; then
+  echo \"$REV\" > ./machines/abusmachine/extension-rev.nix
+else
+  echo "ERROR: unable to update extension-rev.nix"
+fi
+
 popd
